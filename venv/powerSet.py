43b89e83fc2c2af2810ea4a91585551b927ccad7
mithridates++ <fsaulean@gmail.com>
2018-06-22 14:27:39 -0400

    added an alternative version

--- venv/powerSet.py
+++ venv/powerSet.py
@@ -12,6 +12,19 @@ def powerSet(entryArray):
                 finalArray.append(temp)
     return finalArray
 
+def powerset(n):
+    final = [[]]
+    def powerset_helper(array, org_array, index):
+        i = index
+        while i < len(org_array):
+            list_tmp = copy.deepcopy(array)
+            list_tmp.append(org_array[index])
+            final.append(list_tmp)
+            i += 1
+            powerset_helper(list_tmp, org_array, i)
+    
+    powerset_helper([], n, 0)
+    return final 
 
 c = powerSet(["1","2"])
 print(c)
